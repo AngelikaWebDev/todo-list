d6d48c554c79c36a5a232205f0f4ae91442d4623
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-05-07 12:53:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
 document.getElementById('addTareaBtn').addEventListener('click', function() {
     console.log("Botón Añadir tarea clickeado");
-    var modal = new bootstrap.Modal(document.getElementById('addTareaModal'));
+    let modal = new bootstrap.Modal(document.getElementById('addTareaModal'));
     modal.show();
 });
 
 document.getElementById('saveTareaBtn').addEventListener('click', function() {
     console.log("Botón Aceptar clickeado");
-    var titulo = document.getElementById('titulo').value;
-    var descripcion = document.getElementById('descripcion').value;
-    
+    let titulo = document.getElementById('titulo').value;
+    let descripcion = document.getElementById('descripcion').value;
+
     // Obtener la tabla
-    var table = document.getElementById('tareaTable');
+    let table = document.getElementById('tareaTable');
     console.log("Tabla encontrada:", table);
 
     // Obtener el cuerpo de la tabla
-    var tbody = table.getElementsByTagName('tbody')[0];
+    let tbody = table.getElementsByTagName('tbody')[0];
     console.log("Cuerpo de la tabla:", tbody);
 
     // Crear una nueva fila para la tabla
-    var newRow = tbody.insertRow();
+    let newRow = tbody.insertRow();
     console.log("Nueva fila creada:", newRow);
 
     // Insertar celdas con los datos de la tarea
-    var checkboxCell = newRow.insertCell(0);
+    let checkboxCell = newRow.insertCell(0);
     checkboxCell.innerHTML = '<input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">';
-    
-    var titleCell = newRow.insertCell(1);
+
+    let titleCell = newRow.insertCell(1);
     titleCell.textContent = titulo;
-    
-    var descriptionCell = newRow.insertCell(2);
+
+    let descriptionCell = newRow.insertCell(2);
     descriptionCell.textContent = descripcion;
 
-    var actionCell = newRow.insertCell(3);
+    let actionCell = newRow.insertCell(3);
 
-    var editButton = document.createElement('button');
+    let editButton = document.createElement('button');
     editButton.setAttribute('type', 'button');
     editButton.setAttribute('class', 'btn btn-light editTarea');
     editButton.innerHTML = '<i class="fa-solid fa-pen-to-square text-success"></i>';
     actionCell.appendChild(editButton);
 
-    var deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
     deleteButton.setAttribute('type', 'button');
     deleteButton.setAttribute('class', 'btn btn-light deleteTarea');
     deleteButton.innerHTML = '<i class="fa-solid fa-trash-can text-danger"></i>';
     actionCell.appendChild(deleteButton);
 
     // Cerrar el modal después de agregar la tarea
-    var modal = bootstrap.Modal.getInstance(document.getElementById('addTareaModal'));
+    let modal = new bootstrap.Modal(document.getElementById('addTareaModal'));
     modal.hide();
-    
+
     // Limpiar los campos del formulario
     document.getElementById('titulo').value = '';
     document.getElementById('descripcion').value = '';
 });
-
